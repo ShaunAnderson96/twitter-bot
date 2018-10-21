f34546161d1b6bd26627de57dcdf455b13108f9e
Shaun Anderson <shaunanderson@Shauns-MacBook-Pro.local>
2018-10-21 19:09:53 +0100

    update

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: "EWztlN0uY3RtN2IHqc2dnRDba",
-  consumer_secret: "dtqqVsOegSY1hRrARFBBa8h1vbOoOIJST7s6TpcMfLr6XlgNfs",
-  access_token: "1054042338450960386-UsRuBhmL6kYGDJJmy4jrlEeAESPeY7",
-  access_token_secret: "uhUlQ980Y3gslqhsTGbaZ5zaEcZqIuiU8ck5wOl5Yb1GL"
-};
